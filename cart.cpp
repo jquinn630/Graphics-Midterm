ece28482a77048e9f4257e800550bf7100997625
Taylor Seale <tseale@nd.edu>
2012-09-29 20:26:57 -0400

    made some changes to phi rotation… still not fully functional

--- cart.cpp
+++ cart.cpp
@@ -23,27 +23,24 @@ void cart::drawCart(float xa, float xb, float xc, float xd, float ya, float yb, 
 	cartX=xa*step*step*step+xb*step*step+xc*step+xd;
 	cartY=ya*step*step*step+yb*step*step+yc*step+yd;
 	cartZ=za*step*step*step+zb*step*step+zc*step+zd;
-	// previous cart position (used to calculate theta & phi headings)
-	step-=.004;
-	float prevCartX=xa*step*step*step+xb*step*step+xc*step+xd;
-	float prevCartY=ya*step*step*step+yb*step*step+yc*step+yd;
-	float prevCartZ=za*step*step*step+zb*step*step+zc*step+zd;
+	// next cart position (used to calculate theta & phi headings)
+	step+=.004;
+	float nextCartX=xa*step*step*step+xb*step*step+xc*step+xd;
+	float nextCartY=ya*step*step*step+yb*step*step+yc*step+yd;
+	float nextCartZ=za*step*step*step+zb*step*step+zc*step+zd;
 	// calculate cartTheta
-	float deltaX = cartX - prevCartX;
-	float deltaZ = cartZ - prevCartZ;
+	float deltaX = nextCartX - cartX;
+	float deltaY = nextCartY - cartY;
+	float deltaZ = nextCartZ - cartZ;
 	cartTheta = atan(deltaX/deltaZ)*(180.0/PI);
 	// calculate cartPhi
-	float deltaY = cartY - prevCartY;
 	cartPhi = atan(deltaX/deltaY)*(180.0/PI);
 	glPushMatrix();{
 		glColor3f(1.0,0.0,0.0);
-		// rotate for theta
+		// rotate for theta & phi
 		glTranslatef(cartX, cartY, cartZ);
 		glRotatef(cartTheta, 0.0, 1.0, 0.0);
-		glTranslatef(-cartX, -cartY, -cartZ);
-		// rotate for phi
-		glTranslatef(cartX, cartY, cartZ);
-		glRotatef(cartPhi, 1.0, 0.0, 0.0);
+		glRotatef(cartPhi, 0.0, 0.0, 1.0);
 		glTranslatef(-cartX, -cartY, -cartZ);
 		// position cart on track
 		glTranslatef(cartX,cartY,cartZ);
