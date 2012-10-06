667fcd135a5901937cab0caa51a6f610054e7a08
John Quinn <jquinn11@nd.edu>
2012-10-06 14:50:44 -0400

    changed the material color a bit to try to make the changing light more noticeable.

--- cart.cpp
+++ cart.cpp
@@ -23,7 +23,7 @@ float wheelTheta=0;
 
 	//declare materials
 	//rollercoaster body
-	float matdiffCol[4] = { 0.8, 0.0, 0.05 };	// reddish				
+	float matdiffCol[4] = { 0.4,0.0,0.0};	// reddish				
 	float matspecCol[4] = { 1.0, 1.0, 1.0 };					
 	float matambCol[4] = { 0.8, 0.8, 0.8 };
 	material bodyMat(matdiffCol, matspecCol, matambCol, 128.0);
